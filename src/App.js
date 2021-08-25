72e5d8e519b10b900042c4c9c74d03ff582513cc
Rahim Pormah <sas4n.porm4h@gmail.com>
2021-08-25 11:44:56 +0200

    added auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,4 +13,22 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
